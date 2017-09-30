85f9c1bf78cd68e096bbb04937af6f4928836780
Mohammad Rajaul Islam <springapidev@gmail.com>
2017-09-30 11:11:02 +0600

    onclick method re[laced by onClick Listener

--- Ex05-ButtonToast/app/src/main/java/com/coderbd/ex05_buttontoast/MainActivity.java
+++ Ex05-ButtonToast/app/src/main/java/com/coderbd/ex05_buttontoast/MainActivity.java
@@ -15,15 +15,23 @@ private Button btn;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//It is instead of showToast method
         btn=(Button)findViewById(R.id.btn);
         sText=(TextView)findViewById(R.id.show_toast_id);
-
+btn.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Toast toast=Toast.makeText(view.getContext(), R.string.show_toast_msg, Toast.LENGTH_SHORT);
+        toast.show();
     }
+});
+    }
+    /*
+    //If we do not use this method, then no need to declare at activity_main.xml as   android:onClick="showToast"
     public void showToast(View view){
         Toast.makeText(getApplicationContext(),R.string.show_toast_msg, Toast.LENGTH_SHORT).show();
         //remove it after checking
         sText.append("Ok");
     }
-
+*/
 }
