0aba6f06b8ba1fcc48ca4a375b019c19130b9d67
Mohammad Rajaul Islam <springapidev@gmail.com>
2017-10-15 15:39:12 +0600

    update.........

--- Ex07-IntentDataTransfer/app/src/main/java/com/coderbd/ex07_intentdatatransfer/Person.java
+++ Ex07-IntentDataTransfer/app/src/main/java/com/coderbd/ex07_intentdatatransfer/Person.java
@@ -22,4 +22,12 @@ public class Person implements Serializable {
     public int getAge() {
         return age;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
