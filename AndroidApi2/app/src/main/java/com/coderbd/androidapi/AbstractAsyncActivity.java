8ef5c5bf6ef7fef702033c9d38b5a3285328dc93
Mohammad Rajaul Islam <springapidev@gmail.com>
2017-11-26 16:33:27 +0600

    some new project with api

---
+++ AndroidApi2/app/src/main/java/com/coderbd/androidapi/AbstractAsyncActivity.java
@@ -0,0 +1,54 @@
+package com.coderbd.androidapi;
+import android.app.Activity;
+import android.app.ProgressDialog;
+/**
+ * Created by Rajail Islam on 11/26/2017.
+ */
+
+
+
+/**
+ * @author Roy Clarkson
+ * @author Pierre-Yves Ricau
+ */
+public abstract class AbstractAsyncActivity extends Activity {
+
+    protected static final String TAG = AbstractAsyncActivity.class.getSimpleName();
+
+    private ProgressDialog progressDialog;
+
+    private boolean destroyed = false;
+
+    // ***************************************
+    // Activity methods
+    // ***************************************
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        destroyed = true;
+    }
+
+    // ***************************************
+    // Public methods
+    // ***************************************
+    public void showLoadingProgressDialog() {
+        this.showProgressDialog("Loading. Please wait...");
+    }
+
+    public void showProgressDialog(CharSequence message) {
+        if (progressDialog == null) {
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setIndeterminate(true);
+        }
+
+        progressDialog.setMessage(message);
+        progressDialog.show();
+    }
+
+    public void dismissProgressDialog() {
+        if (progressDialog != null && !destroyed) {
+            progressDialog.dismiss();
+        }
+    }
+
+}
