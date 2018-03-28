86a43e1b6acd8a09d98cb97167d54a4521364f38
Mohammad Rajaul Islam <springapidev@gmail.com>
2018-03-28 13:16:21 +0600

    menu ex

---
+++ okay-menu-example/MenuEx/app/src/main/java/com/coderbd/menuex/MainActivity.java
@@ -0,0 +1,57 @@
+package com.coderbd.menuex;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    ListView listView1;
+    String contacts[]={"Shamim","Atik","Ruhul","Shabib","Parvez"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView1=(ListView)findViewById(R.id.listView1ccccc);
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,contacts);
+        listView1.setAdapter(adapter);
+
+        // Register the ListView  for Context menu
+        registerForContextMenu(listView1);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Select The Action");
+        menu.add(0, Menu.FIRST, Menu.NONE, "Call");//groupId, itemId, order, title
+        menu.add(0, Menu.FIRST, Menu.NONE, "SMS");
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        if(item.getTitle()=="Call"){
+            Toast.makeText(getApplicationContext(),"calling code",Toast.LENGTH_LONG).show();
+        Intent intent=new Intent(this, CallActivity.class);
+         startActivity(intent);
+        }
+        else if(item.getTitle()=="SMS"){
+            Toast.makeText(getApplicationContext(),"sending sms code",Toast.LENGTH_LONG).show();
+        }else{
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ okay-menu-example/OptionsMenuEx3/app/src/main/java/com/coderbd/optionsmenuex/MainActivity.java
@@ -0,0 +1,66 @@
+package com.coderbd.optionsmenuex;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast.makeText(getApplicationContext(),"..............wow",Toast.LENGTH_LONG).show();
+            return true;
+        } else if (id == R.id.action_menu2) {
+            Toast.makeText(getApplicationContext(),"..............menu 2",Toast.LENGTH_LONG).show();
+
+            return true;
+        } else if (id == R.id.action_menu3) {
+            Toast.makeText(getApplicationContext(),"..............menu 3",Toast.LENGTH_LONG).show();
+            return true;
+        } else if (id == R.id.action_menu4) {
+            Toast.makeText(getApplicationContext(),"..............menu 4",Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
